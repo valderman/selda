9b0e97ad050f4c2e50cb67bab80ce87ce77aa581
Anton Ekblad <anton@ekblad.cc>
2017-09-08 14:58:23 +0100

    Bump version numbers.

--- selda/default.nix
+++ selda/default.nix
@@ -3,7 +3,7 @@
 }:
 mkDerivation {
   pname = "selda";
-  version = "0.1.10.1";
+  version = "0.1.11.0";
   src = ./.;
   libraryHaskellDepends = [
     base exceptions hashable mtl psqueues text time
